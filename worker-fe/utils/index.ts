1fc10c357a67e92df6bf3cba783ba60f0e73c2b6
Aryan Pachori <aryanpachori03@gmail.com>
2024-07-05 06:49:14 +0530

    Utils update

--- worker-fe/utils/index.ts
+++ worker-fe/utils/index.ts
@@ -1,3 +1,3 @@
 
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = "https://label-chain.onrender.com";
 export const CLOUDFRONT_URL = "https://d1vevpr96rpmyb.cloudfront.net"
